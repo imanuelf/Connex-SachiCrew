e57781ee0e52be022779cb00877275fda99c0035
Muhammad Firdaus <83199676+HexasSS@users.noreply.github.com>
2023-06-04 10:45:52 +0700

    rewards page

---
+++ app/Views/rewards/rewards.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.responsive {
+  width: 100%;
+  height: auto;
+  max-width: 960px;
+}
+</style>
+</head>
+<body>
+
+<img src="http://connex.akreditasi-infokom.org/assets/images/RewardsPage.png" class="responsive" width="200" height="400">
+
+</body>
+</html>
